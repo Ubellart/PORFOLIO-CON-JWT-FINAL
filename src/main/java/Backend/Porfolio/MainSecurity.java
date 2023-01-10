5cfb4168a627c1ec4acfa2fd00217bde664852c9
Ubellart <97629366+Ubellart@users.noreply.github.com>
2023-01-10 19:35:52 -0300

    Update MainSecurity.java

--- src/main/java/Backend/Porfolio/MainSecurity.java
+++ src/main/java/Backend/Porfolio/MainSecurity.java
@@ -1,9 +1,6 @@
 package Backend.Porfolio;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 
 import Backend.Porfolio.Security.Jwt.JwtEntryPoint;
